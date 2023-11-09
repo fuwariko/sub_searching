102b93e1e01637808c68276dcd63cc212a6c820c
Fuwariko <cessedimeow@gmail.com>
2023-11-09 22:24:11 +0500

    add

--- graphics.py
+++ graphics.py
@@ -11,65 +11,65 @@ import aho_corasick
 import z_function
 
 
-def draw_graph_time():
-    methods = [brute_force, knut_morris_pratt, rabin_karp, aho_corasick, z_function]
-    titles = ['Brute Force', 'Knut-Morris-Pratt', 'Rabin-Karp', 'Aho-Corasick', "z-function"]
+def draw_graph_time(method, title):
+    x_time, y_time = [], []
 
-    for method, title in zip(methods, titles):
-        x_time, y_time = [], []
+    for i in range(10, 100000, 10000):
+        time_sum = 0
 
-        for i in range(10, 100000, 10000):
-            time_sum = 0
+        for j in range(1, i, 10000):
+            start_time = time.time()
+            str = ''.join(choice(ascii_letters) for _ in range(i))
+            sub_str = ''.join(choice(ascii_letters) for _ in range(j))
+            method.find_substring(str, sub_str)
+            end_time = time.time()
+            time_sum += ((end_time - start_time) * 10 ** 3)
 
-            for j in range(1, i, 10000):
-                str = ''.join(choice(ascii_letters) for _ in range(i))
-                sub_str = ''.join(choice(ascii_letters) for _ in range(j))
-                start_time = time.time()
-                method.find_substring(str, sub_str)
-                end_time = time.time()
+        x_time.append(i)
+        y_time.append(time_sum)
 
-                time_sum += ((end_time - start_time) * 10 ** 3)
-
-            x_time.append(i)
-            y_time.append(time_sum)
-
-        plt.subplot(2, 1, 1)
-        plt.plot(x_time, y_time, label=title)
-        plt.xlabel('Длина строки')
-        plt.ylabel('Время')
-        plt.legend()
+    plt.subplot(2, 1, 1)
+    plt.plot(x_time, y_time, label=title)
+    plt.xlabel('Длина строки')
+    plt.ylabel('Время')
+    plt.legend()
 
     plt.show()
 
 
-def draw_graph_memory():
-    methods = [brute_force, knut_morris_pratt, rabin_karp, aho_corasick, z_function]
-    titles = ['Brute Force', 'Knut-Morris-Pratt', 'Rabin-Karp', 'Aho-Corasick', "z-function"]
+def draw_graph_memory(method, title):
+    x_memory, y_memory = [], []
 
-    for method, title in zip(methods, titles):
-        x_memory, y_memory = [], []
+    for i in range(10, 100000, 10000):
+        memory_sum = 0
 
-        for i in range(10, 100000, 10000):
-            memory_sum = 0
+        for j in range(1, i, 10000):
+            tracemalloc.start()
+            str = ''.join(choice(ascii_letters) for _ in range(i))
+            sub_str = ''.join(choice(ascii_letters) for _ in range(j))
+            method.find_substring(str, sub_str)
+            memory_sum += tracemalloc.get_traced_memory()[0]
 
-            for j in range(1, i, 10000):
-                str = ''.join(choice(ascii_letters) for _ in range(i))
-                sub_str = ''.join(choice(ascii_letters) for _ in range(j))
-                tracemalloc.start()
-                method.find_substring(str, sub_str)
-                memory_sum += tracemalloc.get_traced_memory()[0]
+        x_memory.append(i)
+        y_memory.append(memory_sum)
 
-            x_memory.append(i)
-            y_memory.append(memory_sum)
-
-        plt.subplot(2, 1, 1)
-        plt.plot(x_memory, y_memory, label=title)
-        plt.xlabel('Длина строки')
-        plt.ylabel('Память')
-        plt.legend()
+    plt.subplot(2, 1, 1)
+    plt.plot(x_memory, y_memory, label=title)
+    plt.xlabel('Длина строки')
+    plt.ylabel('Память')
+    plt.legend()
 
     plt.show()
 
 
-draw_graph_time()
-draw_graph_memory()
+def draw_all_graphs():
+    methods = [brute_force, knut_morris_pratt, rabin_karp, aho_corasick, z_function]
+    titles = ['Brute Force', 'Knut-Morris-Pratt', 'Rabin-Karp', 'Aho-Corasick', "z-function"]
+
+    for method, title in zip(methods, titles):
+        draw_graph_memory(method, title)
+        draw_graph_time(method, title)
+
+
+draw_all_graphs()
+
--- z_function.py
+++ z_function.py
@@ -2,12 +2,16 @@ def z_function(string):
     st_len = len(string)
     z_func = [0] * st_len
     left = 0
-    right = 0
+    right = 0  # границы самой правой подстроки = префиксу, которую уже нашли
     for i in range(1, st_len):
-        if i <= right:
+        if i <= right:  # если символ лежит внутри границ, то значение ф-ии будет от i-left,
+            # если = правой границе, то обрезаем до нее
             z_func[i] = min(right - i + 1, z_func[i - left])
         while i + z_func[i] < st_len and string[z_func[i]] == string[i + z_func[i]]:
+            # обновляем значение зет функции, пока не встретились неравные символы с префиксом
+            # и пока не вышли за границы
             z_func[i] += 1
+        # проверяем, правее ли мы, обновляем границы
         if i + z_func[i] - 1 > right:
             right = i + z_func[i] - 1
             left = i
